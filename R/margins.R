df4aa0f3ea73cd3fbf942775b4181969f78ff88d
Kara Woo <woo.kara@gmail.com>
2017-07-31 12:56:47 -0700

    Rename expand_x and expand_y to margin_x and margin_y

--- R/margins.R
+++ R/margins.R
@@ -23,7 +23,7 @@ margin_width <- function(grob, margins) {
 }
 
 titleGrob <- function(label, x, y, hjust, vjust, angle = 0, gp = gpar(),
-                      margin = NULL, expand_x = FALSE, expand_y = FALSE,
+                      margin = NULL, margin_x = FALSE, margin_y = FALSE,
                       debug = FALSE) {
 
   if (is.null(label))
@@ -71,19 +71,19 @@ titleGrob <- function(label, x, y, hjust, vjust, angle = 0, gp = gpar(),
     children <- gList(text_grob)
   }
  
-  if (expand_x && expand_y) {
+  if (margin_x && margin_y) {
     widths <- unit.c(margin[4], text_width, margin[2])
     heights <- unit.c(margin[1], text_height, margin[3])
 
     vp <- viewport(layout = grid.layout(3, 3, heights = heights, widths = widths), gp = gp)
     child_vp <- viewport(layout.pos.row = 2, layout.pos.col = 2)
-  } else if (expand_x) {
+  } else if (margin_x) {
     widths <- unit.c(margin[4], text_width, margin[2])
     vp <- viewport(layout = grid.layout(1, 3, widths = widths), gp = gp)
     child_vp <- viewport(layout.pos.col = 2)
 
     heights <- unit(1, "null")
-  } else if (expand_y) {
+  } else if (margin_y) {
     heights <- unit.c(margin[1], text_height, margin[3])
 
     vp <- viewport(layout = grid.layout(3, 1, heights = heights), gp = gp)
